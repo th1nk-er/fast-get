2c4bd760a50c665ffa8f361611876d2c1c1bd738
th1nk <vinkangnet@gmail.com>
2023-05-22 23:12:58 +0800

    modified the CORS configuration

--- server/go.mod
+++ server/go.mod
@@ -3,7 +3,6 @@ module github.com/th1nk-er/fast-get/server
 go 1.20
 
 require (
-	github.com/gin-contrib/cors v1.4.0
 	github.com/gin-gonic/gin v1.8.2
 	gopkg.in/yaml.v3 v3.0.1
 )
--- server/main.go
+++ server/main.go
@@ -1,22 +1,10 @@
 package main
 
 import (
-	"github.com/gin-contrib/cors"
 	"github.com/th1nk-er/fast-get/server/routers"
 )
 
 func main() {
 	r := routers.InitRouters()
-
-	// cors middleware
-	corsConfig := cors.Config{
-		AllowOrigins:     []string{"*"},
-		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
-		AllowHeaders:     []string{"*"},
-		AllowCredentials: true,
-	}
-
-	r.Use(cors.New(corsConfig))
-
 	_ = r.Run(":8080")
 }
